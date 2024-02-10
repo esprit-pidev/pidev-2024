e34fd9983ff12d9c807c16d9e50a7fe29df78e7a
Ahmed-Tahry <ahmed.tahri12@outook.fr>
2024-02-10 19:06:46 +0100

    adding project structure

---
+++ src/main/java/tn/esprit/tools/MyDB.java
@@ -0,0 +1,34 @@
+package tn.esprit.tools;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class MyDB {
+
+    public static final String url = "jdbc:mysql://localhost:3306/pidev1";
+    public static final String user = "root";
+    public static final String pwd = "";
+    private Connection cnx;
+    public static MyDB ct;
+
+    private MyDB() {
+        try {
+            this.cnx = DriverManager.getConnection("jdbc:mysql://localhost:3306/pidev1", "root", "");
+            System.out.println("connexion etablie !!");
+        } catch (SQLException var2) {
+            System.out.println(var2.getMessage());
+        }
+
+    }
+
+    public Connection getCnx() {
+        return this.cnx;
+    }
+
+    public static MyDB getInstance() {
+        if (ct == null) {
+            ct = new MyDB();
+        }
+
+        return ct;
+    }
+}
